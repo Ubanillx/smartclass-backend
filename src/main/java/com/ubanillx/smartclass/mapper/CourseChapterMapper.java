6c906ea78264f91a0bde5389046128002ba39fbf
ubanillx <longxin_liu@qq.com>
2025-03-23 11:45:16 +0800

    feat(mapper): 添加 CourseChapterMapper 接口和 XML 配置文件
    - 新增 CourseChapterMapper 接口，用于课程章节的数据库操作
    - 创建 CourseChapterMapper.xml 配置文件，定义课程章节的 SQL 映射
    - 实现了课程章节表的基本 CRUD 功能

---
+++ src/main/java/com/ubanillx/smartclass/mapper/CourseChapterMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.ubanillx.smartclass.model.entity.CourseChapter;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liulo
+* @description 针对表【course_chapter(课程章节)】的数据库操作Mapper
+* @createDate 2025-03-21 15:14:50
+* @Entity com.ubanillx.smartclass.model.entity.CourseChapter
+*/
+public interface CourseChapterMapper extends BaseMapper<CourseChapter> {
+
+}
+
+
+
+
