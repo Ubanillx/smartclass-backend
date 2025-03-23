f3eec0edecf3521eba670dcea9aa6be9b918f3b3
ubanillx <longxin_liu@qq.com>
2025-03-23 11:46:55 +0800

    feat(mapper): 添加用户公告阅读记录 Mapper
    - 新增 UserAnnouncementReaderMapper 接口，用于用户公告阅读记录的数据库操作- 添加 UserAnnouncementReaderMapper.xml 配置文件，定义结果映射和 SQL 语句

---
+++ src/main/java/com/ubanillx/smartclass/mapper/UserAnnouncementReaderMapper.java
@@ -0,0 +1,14 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclass.model.entity.UserAnnouncementReader;
+
+/**
+ * 用户公告阅读记录Mapper
+ */
+public interface UserAnnouncementReaderMapper extends BaseMapper<UserAnnouncementReader> {
+}
+
+
+
+
