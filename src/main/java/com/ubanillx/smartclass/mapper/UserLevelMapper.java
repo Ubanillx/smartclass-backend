49ea80f3272f4c2c8225d56508dea3ac59c36bae
ubanillx <longxin_liu@qq.com>
2025-03-23 11:47:40 +0800

    feat(mapper): 添加用户等级数据库操作接口和映射文件
    - 新增 UserLevelMapper 接口，用于用户等级相关的数据库操作- 创建 UserLevelMapper.xml 映射文件，定义用户等级表的查询结果映射

---
+++ src/main/java/com/ubanillx/smartclass/mapper/UserLevelMapper.java
@@ -0,0 +1,15 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclass.model.entity.UserLevel;
+
+/**
+ * 用户等级数据库操作
+ */
+public interface UserLevelMapper extends BaseMapper<UserLevel> {
+
+}
+
+
+
+
