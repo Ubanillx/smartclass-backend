938d82121931af2222ff86b3dfc403be84f4c153
ubanillx <longxin_liu@qq.com>
2025-03-23 11:46:30 +0800

    feat(database): 添加用户学习统计表的 Mapper
    - 新增 UserLearningStatsMapper 接口，用于用户学习统计表的数据库操作
    - 添加 UserLearningStatsMapper.xml 配置文件，定义表结构和映射关系
    - 该 Mapper继承自 BaseMapper，支持基本的 CRUD 操作

---
+++ src/main/java/com/ubanillx/smartclass/mapper/UserLearningStatsMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.ubanillx.smartclass.model.entity.UserLearningStats;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liulo
+* @description 针对表【user_learning_stats(用户学习统计)】的数据库操作Mapper
+* @createDate 2025-03-21 15:14:50
+* @Entity com.ubanillx.smartclass.model.entity.UserLearningStats
+*/
+public interface UserLearningStatsMapper extends BaseMapper<UserLearningStats> {
+
+}
+
+
+
+
