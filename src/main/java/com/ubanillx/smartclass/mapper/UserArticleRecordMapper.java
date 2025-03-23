b7c8c1afc825ce382f0dea3c3104f2b768cd5cc4
ubanillx <longxin_liu@qq.com>
2025-03-23 11:47:06 +0800

    feat(mapper): 添加 UserArticleRecordMapper 接口和映射文件
    - 新增 UserArticleRecordMapper 接口，用于用户文章阅读记录的数据库操作
    - 创建 UserArticleRecordMapper.xml 映射文件，定义结果集映射和基础 SQL 语句
    - 实现了用户文章阅读记录的 CRUD功能

---
+++ src/main/java/com/ubanillx/smartclass/mapper/UserArticleRecordMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.ubanillx.smartclass.model.dto.UserArticleRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liulo
+* @description 针对表【user_article_record(用户文章阅读记录)】的数据库操作Mapper
+* @createDate 2025-03-20 14:36:38
+* @Entity com.ubanillx.smartclass.model.dto.UserArticleRecord
+*/
+public interface UserArticleRecordMapper extends BaseMapper<UserArticleRecord> {
+
+}
+
+
+
+
