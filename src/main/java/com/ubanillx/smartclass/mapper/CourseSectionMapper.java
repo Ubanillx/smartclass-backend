2016422b61fd78122191d214f70b47a88eab069e
ubanillx <longxin_liu@qq.com>
2025-03-23 11:46:05 +0800

    feat(mapper): 添加 CourseSection 实体的数据库操作 Mapper
    - 新增 CourseSectionMapper 接口，用于课程小节表的数据库操作
    - 创建 CourseSectionMapper.xml 文件，定义课程小节表的结果映射和 SQL 语句

---
+++ src/main/java/com/ubanillx/smartclass/mapper/CourseSectionMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.ubanillx.smartclass.model.entity.CourseSection;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liulo
+* @description 针对表【course_section(课程小节)】的数据库操作Mapper
+* @createDate 2025-03-21 15:14:50
+* @Entity com.ubanillx.smartclass.model.entity.CourseSection
+*/
+public interface CourseSectionMapper extends BaseMapper<CourseSection> {
+
+}
+
+
+
+
