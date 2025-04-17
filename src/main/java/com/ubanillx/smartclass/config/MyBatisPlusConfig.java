7c67f07e0a3b32ce4fd1791849dc76cba887fafb
ubanillx <longxin_liu@qq.com>
2025-04-17 14:12:28 +0800

    refactor: 移除作者和来源注释
    移除了多个文件中的作者和来源注释，包括：
    - .gitignore
    - CodeGenerator.java
    - create_table.sql- Dockerfile
    - MyBatisPlusConfig.java
    - pom.xml- 多个 Mapper.xml 文件这样做可以提高代码的可读性和维护性，同时保持代码的简洁性。

--- src/main/java/com/ubanillx/smartclass/config/MyBatisPlusConfig.java
+++ src/main/java/com/ubanillx/smartclass/config/MyBatisPlusConfig.java
@@ -3,17 +3,14 @@ package com.ubanillx.smartclass.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * MyBatis Plus 配置
  *
- * @author https://github.com/liyupi
  */
 @Configuration
-@MapperScan("com.ubanillx.smartclass.mapper")
 public class MyBatisPlusConfig {
 
     /**
