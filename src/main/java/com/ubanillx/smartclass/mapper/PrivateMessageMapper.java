4229b239e90c158e0b7356bc5111a2dbc8159d81
Ubanillx <liulongxin21@gmail.com>
2025-05-10 19:14:34 +0800

    feat(private-message): 添加私聊消息功能
    - 实现了私聊消息的发送、接收、查询等功能
    - 新增 PrivateMessage 实体类和相关 DTO、VO 类- 创建 PrivateMessageMapper 及其 XML 配置
    - 实现了 PrivateMessageService 接口及其具体实现类

---
+++ src/main/java/com/ubanillx/smartclass/mapper/PrivateMessageMapper.java
@@ -0,0 +1,14 @@
+package com.ubanillx.smartclass.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclass.model.entity.PrivateMessage;
+
+/**
+ * 私聊消息Mapper
+ */
+public interface PrivateMessageMapper extends BaseMapper<PrivateMessage> {
+}
+
+
+
+
